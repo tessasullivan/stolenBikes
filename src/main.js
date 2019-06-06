70fd9e2295c8be9fb60d26b10187d7f4ec724497
Crystal Fecteau and Tessa Sullivan <crystal@>
2019-06-06 14:32:25 -0700

    Moved empty row line to displayLocationPage function

--- src/main.js
+++ src/main.js
@@ -13,12 +13,13 @@ function displayLocationPage(city, page) {
   promise.then(function(response) {
     let body = JSON.parse(response);
     $('#listBikes').removeClass('hide');
-
+    
+    $('tr:not(#tableHeader)').empty();
     body.bikes.forEach(function(bike) {
       let date = new Date(bike.date_stolen*1000);
 
       let stolen_date = `${date.getMonth()}/${date.getDay()}/${date.getFullYear()}`;
-
+      
       $('#listBikes').append(`<tr><td>${stolen_date}</td><td>${bike.stolen_location}</td><td>${bike.title}</td><td>${bike.serial}</td><td>${bike.frame_colors}</td></tr>`);
       },function(error) {
         $('#listBikesError').append(`${error}`);
@@ -47,7 +48,6 @@ $().ready(function(){
     $('#location').val('');
 
     countStolenBikes(city);
-    $('tr:not(#tableHeader)').empty();
     displayLocationPage(city,page);
 
 
